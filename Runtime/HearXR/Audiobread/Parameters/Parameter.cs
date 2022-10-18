cb9ad5498a9785443a1dfd9792ef7df47f542d5b
Anastasia Devana <anastasia.devana@gmail.com>
2022-10-18 18:16:22 -0400

    Nothing special

--- Runtime/HearXR/Audiobread/Parameters/Parameter.cs
+++ Runtime/HearXR/Audiobread/Parameters/Parameter.cs
@@ -5,7 +5,7 @@ namespace HearXR.Audiobread
     /// <summary>
     /// Describes a parameter and its behavior.
     /// </summary>
-    [CreateAssetMenu(fileName = "~~NameThis~~Parameter", menuName = "Audiobread/Parameter", order = 200)]
+    [CreateAssetMenu(fileName = "~~NameThis~~Parameter~PlaceInResourcesFolder", menuName = "Audiobread/Parameter", order = 200)]
     public class Parameter : ScriptableObject
     {
         public float minValue;
