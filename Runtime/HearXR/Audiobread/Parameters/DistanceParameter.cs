95b231c71ebea021d43b6fae8c20792afafe24aa
Anastasia Devana <anastasia.devana@gmail.com>
2023-05-29 12:11:44 -0400

    Error handling

--- Runtime/HearXR/Audiobread/Parameters/DistanceParameter.cs
+++ Runtime/HearXR/Audiobread/Parameters/DistanceParameter.cs
@@ -1,22 +1,36 @@
-// using UnityEngine;
-
+using System;
 using UnityEngine;
 
 namespace HearXR.Audiobread
 {
-    // [CreateAssetMenu (fileName = "BuiltInSoundEvent", menuName = "Audiobread/Built In/Parameter/Distance")]
     public class DistanceParameter : BuiltInParameter
     {
+        #region Private Fields
+        [NonSerialized] private bool _objectLost;
+        [NonSerialized] private float _lastDistance;
+        #endregion
+
+        
         public override float Calculate(ISound sound, SetValuesType setValuesType)
         {
             // Always calculate distance, so we won't check the setValuesType.
-
             var soundSourceObject = sound.SoundSourceObject;
             
+            if (_objectLost) return _lastDistance;
+
             // TODO: Cache Game Object distance values in some central place.
             
-            var distance = Vector3.Distance(ListenerTransform.position, soundSourceObject.transform.position);
-            return distance;
+            try
+            {
+                _lastDistance = Vector3.Distance(ListenerTransform.position, soundSourceObject.transform.position);  
+            }
+            catch (Exception e)
+            {
+                _objectLost = true;
+            }
+
+            return _lastDistance;
+
         }
     }
 }
