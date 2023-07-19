62c06af1843cc73b74cbdecf5dbee75e5d815d55
Anastasia Devana <anastasia.devana@gmail.com>
2023-07-19 16:41:22 -0400

    Make sure we start calculating distance again when a new sound source object is assigned.

--- Runtime/HearXR/Audiobread/Parameters/DistanceParameter.cs
+++ Runtime/HearXR/Audiobread/Parameters/DistanceParameter.cs
@@ -8,21 +8,42 @@ namespace HearXR.Audiobread
         #region Private Fields
         [NonSerialized] private bool _objectLost;
         [NonSerialized] private float _lastDistance;
+        [NonSerialized] private int _lastSoundSourceObjectID;
         #endregion
 
         
         public override float Calculate(ISound sound, SetValuesType setValuesType)
         {
             // Always calculate distance, so we won't check the setValuesType.
-            var soundSourceObject = sound.SoundSourceObject;
+            // Sometimes the game object gets destroyed and trying to access it will create an error.
+            // In that case, return the last known distance.
+            // But don't do that if we have a new sound source object.
+            GameObject soundSourceObject;
+            try
+            {
+                soundSourceObject = sound.SoundSourceObject;
+                var soundSourceObjectID = soundSourceObject.GetInstanceID();
+                if (soundSourceObjectID != _lastSoundSourceObjectID)
+                {
+                    _lastSoundSourceObjectID = soundSourceObjectID;
+                    _objectLost = false;
+                }
+            }
+            catch (Exception e)
+            {
+                // This means that the object does not exist.
+                return _lastDistance;
+            }
             
             if (_objectLost) return _lastDistance;
 
             // TODO: Cache Game Object distance values in some central place.
+            // TODO: Get rid of the try / catch statements and see why we may be trying to calculate the distance
+            //       parameter a bunch when the sound should be stopped.
             
             try
             {
-                _lastDistance = Vector3.Distance(ListenerTransform.position, soundSourceObject.transform.position);  
+                _lastDistance = Vector3.Distance(ListenerTransform.position, soundSourceObject.transform.position);
             }
             catch (Exception e)
             {
